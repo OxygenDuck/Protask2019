8b905cc7531d7e1de8fb8c25e8424d103d9b6f46
student79205 <79205@rocteraa-student.nl>
2020-01-08 10:35:40 +0100

    Fixed world display

--- ProtaskRPG/Assets/Scripts/WorldController.cs
+++ ProtaskRPG/Assets/Scripts/WorldController.cs
@@ -7,7 +7,7 @@ public class WorldController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(Resources.Load<GameObject>("Prefabs/OverworldCharacter"));
+        Instantiate(Resources.Load<GameObject>("Prefabs/OverworldCharacter"), new Vector3(0,0,-0.5f), new Quaternion());
     }
 
     // Update is called once per frame
