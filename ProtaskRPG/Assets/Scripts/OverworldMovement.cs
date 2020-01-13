bc5a38d9bcbae548c0a039066ed8ea966a90ac12
alexander <alexander-deelen@hotmail.com>
2020-01-13 12:28:03 +0100

    added pause menu and changed player spawn location

--- ProtaskRPG/Assets/Scripts/OverworldMovement.cs
+++ ProtaskRPG/Assets/Scripts/OverworldMovement.cs
@@ -11,29 +11,34 @@ public class OverworldMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameObject.Find("Main Camera").transform.position = transform.position;
+        GameObject.Find("Main Camera").transform.position += new Vector3(0, 0, -0.5f);
         GameObject.Find("Main Camera").transform.SetParent(transform); //Set main camera to this object
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Movement
-        if (Input.GetKey(KeyCode.W))//go up
+        if(PauseMenu.gameIsPausedAdee != true)
         {
-                transform.position += new Vector3(0, worldSpeed);      
-        }
-        else if (Input.GetKey(KeyCode.S))//go down
-        {
-            transform.position += new Vector3(0, -worldSpeed);
-        }
+            //Movement
+            if (Input.GetKey(KeyCode.W))//go up
+            {
+                transform.position += new Vector3(0, worldSpeed);
+            }
+            else if (Input.GetKey(KeyCode.S))//go down
+            {
+                transform.position += new Vector3(0, -worldSpeed);
+            }
 
-        if (Input.GetKey(KeyCode.A))//go left
-        {
-            transform.position += new Vector3(-worldSpeed, 0);
-        }
-        else if (Input.GetKey(KeyCode.D))//go right
-        {
-            transform.position += new Vector3(worldSpeed, 0);
-        }
+            if (Input.GetKey(KeyCode.A))//go left
+            {
+                transform.position += new Vector3(-worldSpeed, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))//go right
+            {
+                transform.position += new Vector3(worldSpeed, 0);
+            }
+        }      
     }
 }
---
+++ ProtaskRPG/Assets/Scripts/menus/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameIsPausedAdee = false;
+
+    public GameObject PauseMenuUIAdee;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPausedAdee)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        PauseMenuUIAdee.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPausedAdee = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuUIAdee.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPausedAdee = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("start");
+        Time.timeScale = 1f;
+        gameIsPausedAdee = false;
+    }
+}
