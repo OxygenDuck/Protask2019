37fbc057abb99bd2dfe4cea031ad76f0ca2efbed
student79205 <79205@rocteraa-student.nl>
2020-01-13 13:02:43 +0100

    More work on battle system

---
+++ BattleSystemTest/Assets/BattleHUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHUD : MonoBehaviour
+{
+    public Text HUDText;
+    public Slider HpSlider;
+
+    public void SetHUD(Unit unit)
+    {
+        HUDText.text = unit.unitName + " LV: " + unit.unitLevel;
+        HpSlider.maxValue = unit.maxHp;
+        HpSlider.value = unit.currentHp;
+    }
+
+    public void SetHP(int HP)
+    {
+        HpSlider.value = HP;
+    }
+}
---
+++ BattleSystemTest/Assets/BattleSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState state;
+
+    public GameObject PlayerPrefab;
+    public GameObject EnemyPrefab;
+
+    Unit PlayerUnit;
+    Unit EnemyUnit;
+
+    public Text DialogueText;
+
+    public BattleHUD PlayerHUD;
+    public BattleHUD EnemyHUD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        GameObject PlayerObject = Instantiate(PlayerPrefab, new Vector3(-5, 0), new Quaternion());
+        PlayerUnit = PlayerObject.GetComponent<Unit>();
+        PlayerHUD.SetHUD(PlayerUnit);
+
+        GameObject EnemyObject = Instantiate(EnemyPrefab, new Vector3(5, 0), new Quaternion());
+        EnemyUnit = EnemyObject.GetComponent<Unit>();
+        EnemyHUD.SetHUD(EnemyUnit);
+
+        DialogueText.text = "A wild " + EnemyUnit.unitName + " has appeared!";
+    }
+}
