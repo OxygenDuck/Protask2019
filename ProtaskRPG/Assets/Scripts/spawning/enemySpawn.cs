b69e817f0300a7a7a962c60e6edd0eb1c82e177d
alexander <alexander-deelen@hotmail.com>
2020-01-14 11:32:24 +0100

    added collision and enemy spawning

---
+++ ProtaskRPG/Assets/Scripts/spawning/enemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawnEnemy()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            Instantiate(Resources.Load<GameObject>("Prefabs/Crab"), new Vector3(Random.Range(0, World.instance.widthAdee), Random.Range(0, World.instance.heightAdee), -0.5f), new Quaternion());
+        }
+    }
+}
